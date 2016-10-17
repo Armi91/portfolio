5cfc437fd660bc2cf1f8ea44d4a393a39f39474c
Armi91 <daw.armatys@gmail.com>
2016-10-17 17:53:49 +0200

    Build version

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,35 @@ module.exports = function(grunt){
 				browsers: ['last 2 version']
 			}
 		},
+		cssmin: {
+		  options: {
+		    shorthandCompacting: false,
+		    roundingPrecision: -1
+		  },
+		  target: {
+		    files: {
+		      'build/css/main.css': ['src/owl-carousel/*.css', 'src/css/main.css'],
+		      'build/css/font-awesome.css': ['src/css/font-awesome.css']
+		    }
+		  }
+		},
+		uglify: {
+		    my_target: {
+		      files: {
+		        'build/scripts/scripts.js': ['src/owl-carousel/owl.carousel.js', 'src/scripts/*.js']
+		      }
+		    }
+		  },
+		  imagemin: {
+		    dynamic: {
+		      files: [{
+		        expand: true,
+		        cwd: 'src/img',
+		        src: ['**/*.{png,jpg,gif}'],
+		        dest: 'build/img'
+		      }]
+		    }
+		  },
 		watch: {
 			options: {
 				livereload: true
@@ -32,8 +61,12 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+
 
 	grunt.registerTask('compileLess', ['less', 'autoprefixer']);
-
+	grunt.registerTask('build', ['compileLess', 'cssmin', 'uglify', 'imagemin'])
 
 };
